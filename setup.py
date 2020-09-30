d853eb66087337085cabbb573bdcf4c73a54bb14
toxic-recker <62395124+toxicrecker@users.noreply.github.com>
2020-09-30 19:24:11 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="setup.py",
+    name="similar.py",
     version="1.0",
     author="toxicrecker",
     description="Find similar text easily",
